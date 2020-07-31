691ad09abaa5b47f685f9d3cb59adb6cd8b6a0b3
Rohan Verma <43259200+rohanvrm@users.noreply.github.com>
2020-07-31 13:22:57 +0530

    Add files via upload

---
+++ vibration.ino
@@ -0,0 +1,14 @@
+const int motorPin = 13;
+
+void setup()
+{
+pinMode(motorPin, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(motorPin, HIGH);
+delay(1000);
+digitalWrite(motorPin, LOW);
+delay(1000);
+}
